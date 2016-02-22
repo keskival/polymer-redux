1494fffb041aa40b8a724731ad8b186f5bddc161
Tero Keski-Valkama <tero.keski-valkama@cybercom.com>
2016-02-22 23:36:35 +0200

    More cleanup.

--- components/app-reducer.js
+++ components/app-reducer.js
@@ -1,38 +1,39 @@
 (() => {
   'use strict';
-  function updateGridState(state) {
-    let morePages = false,
-      prevPages = false;
-    const peoplePerPage = 20;
-    const numPages = Math.ceil(state.people.length / peoplePerPage);
-    const pages = [];
-    const from = Math.max(1, state.grid.page - 3);
-    const to = Math.min(numPages, state.grid.page + 3);
-    for (let i=from; i<=to; i++) {
-      pages.push(i);
-    }
-    if (to < numPages) {
-      morePages = true;
-    }
-    if (from > 1) {
-      prevPages = true;
-    }
-    const first = (state.grid.page - 1) * peoplePerPage;
-    const last = Math.min(state.people.length, first + peoplePerPage);
-    return Object.assign({}, state, {
-      grid: Object.assign({}, state.grid, {
-        morePages: morePages,
-        prevPages: prevPages,
-        rows: state.people.slice(first, last + 1),
-        pages: pages
-      })
-    });
-  }
-
   class AppReducer {
     beforeRegister() {
       this.is = 'app-reducer';
     }
+
+    updateGridState(state) {
+      let morePages = false,
+        prevPages = false;
+      const peoplePerPage = 20;
+      const numPages = Math.ceil(state.people.length / peoplePerPage);
+      const pages = [];
+      const from = Math.max(1, state.grid.page - 3);
+      const to = Math.min(numPages, state.grid.page + 3);
+      for (let i=from; i<=to; i++) {
+        pages.push(i);
+      }
+      if (to < numPages) {
+        morePages = true;
+      }
+      if (from > 1) {
+        prevPages = true;
+      }
+      const first = (state.grid.page - 1) * peoplePerPage;
+      const last = Math.min(state.people.length, first + peoplePerPage);
+      return Object.assign({}, state, {
+        grid: Object.assign({}, state.grid, {
+          morePages: morePages,
+          prevPages: prevPages,
+          rows: state.people.slice(first, last + 1),
+          pages: pages
+        })
+      });
+    }
+
     reduce(state, action) {
       const type = action.type,
         data = action.data;
@@ -42,18 +43,18 @@
       switch (type) {
         case 'init':
           console.log(JSON.stringify(data));
-          return updateGridState(
+          return this.updateGridState(
             Object.assign({}, state, data)
           );
         case 'addPerson':
-          return updateGridState(
+          return this.updateGridState(
             Object.assign({}, state, {
             people: [
               ...state.people,
               data
             ]}));
         case 'selectPage':
-          return updateGridState(
+          return this.updateGridState(
             Object.assign({}, state, {
             grid: {
               page: data
@@ -61,7 +62,7 @@
           }));
         case 'deletePerson':
           const people = state.people.filter(person => (person.id != data));
-          return updateGridState(
+          return this.updateGridState(
             Object.assign({}, state, {
             people: people
           }));
